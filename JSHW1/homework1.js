5ed7c8fcf451217f99874d0fcefb09d1ba907d22
DImaValid <115936575+DImaValid@users.noreply.github.com>
2022-11-01 01:07:28 +0300

    1 дз по дс

---
+++ JSHW1/homework1.js
@@ -0,0 +1,32 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//   Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+let a = 'hello'
+let b = 'owu'
+let c = 'com'
+let d = 'ua'
+let g = '-999'
+let f = '123'
+let i = '3.14'
+let w = '2.7'
+let q='2.7'
+let s='16'
+let v='true'
+let e='false'
+console.log(a,b,c,d,g,f,i,w,q,s,v,e)
+let firstName='Dima'
+let midleName='Homenyuk'
+let lastName="Mykolayovych"
+console.log(conc=firstName, midleName, lastName)
+
+
+let p = 100;
+let o = '100';
+let k = true;
+console.log(typeof p)
+console.log(typeof o)
+console.log(typeof k)
